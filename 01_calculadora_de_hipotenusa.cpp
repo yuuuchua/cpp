03bd2fe634098193c07473222f34f92afc23a34f
yuuuchua <marialicermartins44@gmail.com>
2026-02-09 20:47:49 -0300

    starting

---
+++ 01_calculadora_de_hipotenusa.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+int main ()
+{
+    double cat1;
+    double cat2;
+    double hipotenusa;   
+
+    std::cout << R"(Calculadora de hipotenusa
+
+            |\
+            | \
+            |  \
+            |   \
+       cat1 |    \ hipotenusa
+            |     \
+            |      \
+            |       \
+            ----------
+               cat2 )" << "\n\n";
+
+    std::cout << "Digite o valor de \"cat1\" e \"cat2\"" << '\n';
+
+    std::cout << "cat1 = ";
+    std::cin >> cat1;
+
+    std::cout << "cat2 = ";
+    std::cin >> cat2;
+
+
+    hipotenusa = sqrt(cat1*cat1 + cat2*cat2);
+
+    std::cout << "A hipotenusa sera " << hipotenusa << "!" << '\n';
+    return 0;
+}
